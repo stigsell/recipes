b07a84333f571078fb72f64e1538b1ae30fd9439
Nick Stigsell <nick@stigsell.com>
2019-03-01 16:32:06 -0500

    Moved Content and Footer to own components (CSS styling not working on those components), added Roboto font

---
+++ src/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing.unit * 6,
+  },
+});
+
+class Footer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.classes = styles;
+	}
+	render() {
+		return (
+			<footer className={this.classes.footer}>
+		        <Typography variant="h6" align="center" gutterBottom>
+		          Recipes
+		        </Typography>
+		        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+		          Created by Nick Stigsell.
+		        </Typography>
+		    </footer>
+		)
+	}
+}
+
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Footer);
