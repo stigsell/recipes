46e8f6fcdc8b4f8808e1faaaa28c92c49b8f43e3
Nick Stigsell <nick@stigsell.com>
2019-03-02 10:40:26 -0500

    Move RecipeCard into its own component that receives data from parent. Also renamed Content component to Home

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import Content from './Content'
+import Home from './Home'
 import AddRecipe from './AddRecipe'
 // import other components that will be routed to
 
 const Main = () => (
   <main>
     <Switch>
-      <Route exact path='/' component={Content}/>
+      <Route exact path='/' component={Home}/>
       <Route path='/add-recipe' component={AddRecipe}/>
     </Switch>
   </main>
